e0bbd99bfd75101e1c760fd2072feebcf95fde09
hirotk <hiro_tk@outlook.com>
2014-08-16 11:01:05 +0900

    Added a Question model

--- VocabInstaller/Controllers/HomeController.cs
+++ VocabInstaller/Controllers/HomeController.cs
@@ -3,13 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using VocabInstaller.Models;
 
 namespace VocabInstaller.Controllers {
     public class HomeController : Controller {
         public ActionResult Index() {
-            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+//            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+            var questions = new List<Question>(){
+                new Question(){Id=1, Word="word1", Meaning="meaning1"},
+                new Question(){Id=2, Word="word2", Meaning="meaning2"},
+                new Question(){Id=3, Word="word3", Meaning="meaning3"},
+                new Question(){Id=4, Word="word4", Meaning="meaning4"},
+                new Question(){Id=5, Word="word5", Meaning="meaning5"},
+            };
 
-            return View();
+            return View(questions);
         }
 
         public ActionResult About() {
---
+++ VocabInstaller/Models/Question.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace VocabInstaller.Models {
+    public class Question {
+        public int Id { get; set; }
+
+        public int UserId { get; set; }
+
+        [Required]
+        [StringLength(maximumLength: 128)]
+        public string Word { get; set; }        
+        
+        [Required]
+        [StringLength(maximumLength: 256)]
+        public string Meaning { get; set; }
+
+        [StringLength(maximumLength: 512)]
+        public string Note { get; set; }
+
+        public DateTime RegisteredDate { get; set; }
+    }
+}
